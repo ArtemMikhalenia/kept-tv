dbbb40e573520c55b3c92bbb5499c3ab2630a6ba
Artem Mikhalenia <goldtree3000@gmail.com>
2025-11-23 22:14:18 +0300

    feat: add some styles and background to greetings page

--- src/pages/GreetingsPage/GreetingsPage.tsx
+++ src/pages/GreetingsPage/GreetingsPage.tsx
@@ -14,17 +14,20 @@ const GreetingsPage = () => {
         snowflakeCount={500}
         speed={[1, 5]}
         radius={[1, 1.5]}
+        style={{ zIndex: 5 }}
       />
       <MouseParallaxContainer
         globalFactorX={0.1}
         globalFactorY={0.1}
         containerStyle={{ height: '100%' }}
       >
-        <MouseParallaxChild factorX={0.1} factorY={0.1}>
+        <MouseParallaxChild factorX={0.4} factorY={0.4}>
           <div className="greetings-page-content">
             <h1>С Новым годом!</h1>
           </div>
         </MouseParallaxChild>
+        <MouseParallaxChild className="image1" factorX={0.15} factorY={0.05} />
+        <MouseParallaxChild className="image2" factorX={0.15} factorY={0.05} />
       </MouseParallaxContainer>
     </section>
   );
